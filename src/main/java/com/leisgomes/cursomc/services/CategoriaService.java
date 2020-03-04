48f034cbd8e19b09abe3866b4d0da26a7da0cedc
Leís Gomes <leissantoss@gmail.com>
2020-03-04 12:52:38 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/leisgomes/cursomc/services/CategoriaService.java
+++ src/main/java/com/leisgomes/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leisgomes.cursomc.domain.Categoria;
+import com.leisgomes.cursomc.dto.CategoriaDTO;
 import com.leisgomes.cursomc.repositories.CategoriaRepository;
 import com.leisgomes.cursomc.services.exceptions.DataIntegrityException;
 import com.leisgomes.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
